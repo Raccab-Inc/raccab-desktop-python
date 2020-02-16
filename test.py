1299246719f2d6789e9d6848b74e5f57c2940e94
TheCabbar <thecabbarpr@gmail.com>
2020-02-16 03:18:52 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,27 +1,9 @@
-# !/usr/bin/python3
-from tkinter import *
+#!/usr/bin/python3
 
-from tkinter import messagebox
+def KelvinToFahrenheit(Temperature):
+   assert (Temperature >= 0),"Colder than absolute zero!"
+   return ((Temperature-273)*1.8)+32
 
-root = Tk()
-windowWidth = root.winfo_reqwidth()
-windowHeight = root.winfo_reqheight()
-positionRight = int(root.winfo_screenwidth()/2 - windowWidth/2)
-positionDown = int(root.winfo_screenheight()/2 - windowHeight/2)
-root.geometry("360x360+{}+{}".format(positionRight, positionDown))
-
-var1 = 0
-def counter():
-    global var1    
-    var1 += 1
-    var.set(var1)    
-
-var = StringVar()
-label = Label( root, textvariable = var, relief = RAISED )  
-label.pack()
-
-B = Button(root, text = "Increment", command = counter)
-B.pack()
-
-root.title("Simple Prog")
-root.mainloop()
+print (KelvinToFahrenheit(273))
+print (int(KelvinToFahrenheit(505.78)))
+print (KelvinToFahrenheit(-5))
